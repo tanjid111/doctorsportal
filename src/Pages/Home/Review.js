3ff59440b15a6cda582c904246f11745fd0e3702
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 19:41:36 -0700

    review updated

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 const Review = ({ review }) => {
-
     return (
-        <div className="card lg:max-w-lg bg-primary text-primary-content">
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur et fugiat dolorum error dignissimos?</p>
-                <div className='flex items-center'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, voluptates quo incidunt harum minus mollitia!</p>
+                <div className="flex items-center">
                     <div className="avatar">
-                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mr-5">
+                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
                             <img src={review.img} alt="" />
                         </div>
                     </div>
                     <div>
                         <h4 className='text-xl'>{review.name}</h4>
-                        <p className='text-white text-xl'>{review.location}</p>
+                        <p>{review.location}</p>
                     </div>
                 </div>
             </div>
