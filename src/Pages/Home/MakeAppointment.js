e697bea92da532cd6cfa0409bde0bb6a6c6eca79
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 19:41:17 -0700

    make appointment updated

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import doctor from '../../assets/images/doctor.png'
-import appointment from '../../assets/images//appointment.png'
+import doctor from '../../assets/images/doctor.png';
+import appointment from '../../assets/images/appointment.png';
 import PrimaryButton from '../Shared/PrimaryButton';
 
 const MakeAppointment = () => {
@@ -9,14 +9,13 @@ const MakeAppointment = () => {
             background: `url(${appointment})`
         }}
             className='flex justify-center items-center'>
-
             <div className='flex-1 hidden lg:block'>
-                <img className='mt-[-100px]' src={doctor} alt="" srcSet="" />
+                <img className='mt-[-100px]' src={doctor} alt="" />
             </div>
             <div className='flex-1'>
                 <h3 className='text-xl text-primary font-bold'>Appointment</h3>
-                <h2 className='text-3xl text-white'>Make  an Appointment Today</h2>
-                <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab maiores ipsam odio corrupti itaque possimus autem hic quasi quas dolore commodi magnam accusamus, labore deleniti a ea adipisci quibusdam nemo, velit officia, tempora id. Est quidem, amet architecto quo atque eum neque, illo quam laborum pariatur obcaecati cum, consectetur nulla.</p>
+                <h2 className='text-3xl text-white'>Make an Appointment Today</h2>
+                <p className='text-white'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente earum ab cupiditate autem accusantium expedita sequi, temporibus, aut illo doloribus quaerat explicabo, assumenda consectetur est vel ad sed maiores doloremque consequatur. Amet consequuntur quibusdam autem, quod maxime qui itaque quaerat.</p>
                 <PrimaryButton>Get Started</PrimaryButton>
             </div>
         </section>
