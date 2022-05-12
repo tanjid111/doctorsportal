4b7200cc2abcb305f77539d4a953434fcbc4279b
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 19:42:11 -0700

    services updated

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import fluoride from '../../assets/images/fluoride.png'
-import cavity from '../../assets/images/cavity.png'
-import whitening from '../../assets/images/whitening.png'
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
 import Service from './Service';
 
 const Services = () => {
@@ -9,37 +9,34 @@ const Services = () => {
         {
             _id: 1,
             name: 'Fluoride Treatment',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            description: '',
             img: fluoride
         },
         {
             _id: 2,
             name: 'Cavity Filling',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            description: '',
             img: cavity
         },
         {
             _id: 3,
             name: 'Teeth Whitening',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            description: '',
             img: whitening
-        }
-    ]
-
+        },
+    ];
     return (
         <div className='my-28'>
-            <div className='text-center '>
-                <h3 className='text-primary text-xl font-bold uppercase'> Our Services</h3>
+            <div className='text-center'>
+                <h3 className='text-primary  text-xl font-bold uppercase'>Our Services</h3>
                 <h2 className='text-4xl'>Services We Provide</h2>
-
             </div>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-10'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
                 {
                     services.map(service => <Service
                         key={service._id}
                         service={service}
                     ></Service>)
-
                 }
             </div>
         </div>
