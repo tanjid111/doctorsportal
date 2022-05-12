64c425c5216ac29ef89477089393547e3ba9f461
tanjid111 <tanjidhossain@gmail.com>
2022-05-12 00:26:00 -0700

    update

--- src/Pages/Shared/PrimaryButton.js
+++ src/Pages/Shared/PrimaryButton.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const PrimaryButton = ({ children }) => {
     return (
-        <div>
-            <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to primary">{children}</button>
-        </div>
+        <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">{children}</button>
     );
 };
 
