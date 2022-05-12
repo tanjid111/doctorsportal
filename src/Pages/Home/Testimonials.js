b06a82407324f36e667c94bab9c5be14c61d5b94
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 19:42:34 -0700

    testimonials updated

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import quote from '../../assets/icons/quote.svg'
+import quote from '../../assets/icons/quote.svg';
 import people1 from '../../assets/images/people1.png'
 import people2 from '../../assets/images/people2.png'
 import people3 from '../../assets/images/people3.png'
 import Review from './Review';
-
 const Testimonials = () => {
     const reviews = [
         {
             _id: 1,
-            name: 'Winson Henry',
+            name: 'Winson Herry',
             review: '',
-            location: 'California',
+            location: 'california',
             img: people1
         },
         {
             _id: 2,
-            name: 'Winson Henry',
+            name: 'Winson Herry',
             review: '',
-            location: 'California',
+            location: 'california',
             img: people2
         },
         {
             _id: 3,
-            name: 'Winson Henry',
+            name: 'Winson Herry',
             review: '',
-            location: 'California',
+            location: 'california',
             img: people3
-        }
-    ]
+        },
+    ];
     return (
         <section className='my-28'>
             <div className='flex justify-between'>
                 <div>
-                    <h4 className='text-xl text-primary font-bold'>Testimonials</h4>
+                    <h4 className="text-xl text-primary font-bold">Testimonials</h4>
                     <h2 className='text-3xl'>What our Patients say</h2>
                 </div>
                 <div>
