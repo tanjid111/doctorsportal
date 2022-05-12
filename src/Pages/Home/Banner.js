603d9f4d5481a38ba7db69b3148f09e5fed297f2
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 19:34:49 -0700

    banner updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import chair from '../../assets/images/chair.png'
+import chair from '../../assets/images/chair.png';
 import PrimaryButton from '../Shared/PrimaryButton';
 
 const Banner = () => {
@@ -8,13 +8,12 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <PrimaryButton>Get Started</PrimaryButton>
                 </div>
             </div>
         </div>
-
     );
 };
 
