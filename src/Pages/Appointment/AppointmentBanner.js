ad3f683c46ab7e61d088737061be84a2ff8aecaf
tanjid111 <tanjidhossain@gmail.com>
2022-05-11 22:32:43 -0700

    fake data loaded

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
-const AppointmentBanner = () => {
+
+const AppointmentBanner = ({ date, setDate }) => {
+
+
     return (
         <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <DayPicker
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                    />
                 </div>
             </div>
+
         </div>
     );
 };
