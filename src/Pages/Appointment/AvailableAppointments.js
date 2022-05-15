f45f749a75259242b3924a1eac88b22a6990a800
tanjid111 <tanjidhossain@gmail.com>
2022-05-15 12:55:16 -0700

    dashboard added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,25 +1,36 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 import BookingModal from './BookingModal';
 import Service from './service'
 
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    //replaced with react query
+    // const [services, setServices] = useState([]);
     const [treatment, setTreatment] = useState(null);
     const formattedDate = format(date, 'PP');
 
-    useEffect(() => {
-        // fetch('http://localhost:5000/service') change it to available
-        fetch(`http://localhost:5000/available?date=${formattedDate}`)
-            .then(res => res.json())
-            .then(data => setServices(data))
-    }, [formattedDate])
+    const { data: services, isLoadinng, refetch } = useQuery(['available', formattedDate], () => fetch(`http://localhost:5000/available?date=${formattedDate}`)
+        .then(res => res.json())
+    )
+    if (isLoadinng) {
+        return <Loading></Loading>
+    }
+
+    //replaced with react query
+    // useEffect(() => {
+    //     // fetch('http://localhost:5000/service') change it to available
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data))
+    // }, [formattedDate])
     return (
         <div>
             <h4 className='text-xl text-center text-secondary'> Available appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
@@ -30,6 +41,7 @@ const AvailableAppointments = ({ date }) => {
                 date={date}
                 treatment={treatment}
                 setTreatment={setTreatment}
+                refetch={refetch}
             ></BookingModal>}
         </div>
     );
---
+++ src/Pages/Dashboard/MyHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyHistory = () => {
+    return (
+        <div>
+            <h2>This is my Treatment History</h2>
+        </div>
+    );
+};
+
+export default MyHistory; <h2>This is my Treatment History</h2>
