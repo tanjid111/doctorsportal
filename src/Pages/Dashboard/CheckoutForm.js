91aaf05a36c68d5e8f3bc633fad63d3f7a071cdf
tanjid111 <tanjidhossain@gmail.com>
2022-05-18 23:48:24 -0700

    stripe payment added

---
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { CardElement, Elements, useElements, useStripe } from '@stripe/react-stripe-js';
+
+
+const CheckoutForm = ({ appointment }) => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('')
+
+    const { price } = appointment;
+
+    useEffect(() => {
+        fetch('http://localhost:5000/create-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                //content type can also be sent but not necessary
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ price })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret);
+                }
+            })
+    }, [price])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (!stripe || !elements) {
+            return;
+        }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
+            return;
+        }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+
+        setCardError(error?.message || '')
+
+        // or this way
+        // if (error) {
+        //     // console.log(error);
+        //     setCardError(error.message)
+        // }
+        // else {
+        //     setCardError('');
+        // }
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+        </>
+    );
+};
+
+export default CheckoutForm;
---
+++ src/Pages/Dashboard/Payment.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import Loading from '../Shared/Loading';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+    CardElement,
+    Elements,
+    useStripe,
+    useElements,
+} from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L11h4AO0LSxhdmBt0cDEKyHishTX082T1qqYJXJKYF8SSXNh8eX7dWq8kZPkfwlW8znYFM4RlJZwQHeYq8kfAng00xkGVldHC');
+
+const Payment = () => {
+    const { id } = useParams();
+    const url = `http://localhost:5000/booking/${id}`
+
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            //content type can also be sent but not necessary
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    return (
+        <div>
+
+            <div class="card max-w-md w-50 bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <p className="text-success font-bold">Hello, {appointment.patientName}</p>
+                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <p>Your appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
+                    <p>Please pay ${appointment.price}</p>
+                </div>
+            </div>
+
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm
+                            appointment={appointment} />
+                    </Elements>
+
+
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Payment;
